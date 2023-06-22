6fc3512992b52ee0227b2fed05e1ecf1691ac374
lastemp <emmanuelochieng797@gmail.com>
2023-06-22 16:13:05 +0300

    2nd commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 import fetch, { Headers } from 'node-fetch'
 
+export default function Home() {
+  return (
+    <form>
+      <p><label htmlFor="message">Message</label></p>
+      <p><input type="text" id="message" name="message" required /></p>
+      <p><label htmlFor="response">Response</label></p>
+      <p><input type="text" id="response" name="response" readOnly={true} /></p>
+      <p><button type="submit">click to submit chat</button></p>
+  </form>
+  )
+}
+
